a5b1d3890d338aad5f2c88a3a9405273069eec7e
sunchengzhu <sunchengzhu@cryptape.com>
2024-01-30 22:47:46 +0800

    调试

--- get_diff.sh
+++ get_diff.sh
@@ -12,21 +12,21 @@ else
     localhost_number=$((16#$localhost_hex_number))
 fi
 
-# 获取mainnet_hex_number
+# 获取mainnet或testnnet hex_number
 hex_number=$(curl -sS -X POST -H "Content-Type: application/json" -d '{"id": 1, "jsonrpc": "2.0", "method": "get_tip_header", "params": []}' https://${env}.ckbapp.dev | jq -r '.result.number' | sed 's/^0x//')
-if [[ $? -ne 0 || -z "$mainnet_hex_number" ]]; then
+if [[ $? -ne 0 || -z "$hex_number" ]]; then
     number="获取失败"
 else
     number=$((16#$hex_number))
 fi
 
 # 计算差值或指出无法计算
-if [[ $localhost_number =~ ^[0-9]+$ && $mainnet_number =~ ^[0-9]+$ ]]; then
-    difference=$(($mainnet_number - $localhost_number))
+if [[ $localhost_number =~ ^[0-9]+$ && $number =~ ^[0-9]+$ ]]; then
+    difference=$(($number - $localhost_number))
     if [[ $difference -lt 0 ]]; then
         difference=$((-$difference)) # 转换为绝对值
     fi
-    sync_rate=$(echo "scale=10; $localhost_number * 100 / $mainnet_number" | bc | awk '{printf "%.2f\n", $0}')
+    sync_rate=$(echo "scale=10; $localhost_number * 100 / $number" | bc | awk '{printf "%.2f\n", $0}')
     sync_rate="${sync_rate}%"
 else
     difference="无法计算"
