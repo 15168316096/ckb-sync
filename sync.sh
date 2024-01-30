75bc22528d1909a2f0cf4b31cf3aeb7c6743faa8
sunchengzhu <sunchengzhu@cryptape.com>
2024-01-30 22:18:56 +0800

    调试

--- sync.sh
+++ sync.sh
@@ -14,7 +14,7 @@ if [ ! -f "$tar_name" ]; then
     wget "https://github.com/nervosnetwork/ckb/releases/download/${ckb_version}/${tar_name}"
 fi
 
-rm -rf ckb_*_x86_64-unknown-linux-gnu
+sudo rm -rf ckb_*_x86_64-unknown-linux-gnu
 tar xzvf ${tar_name}
 cd ckb_${ckb_version}_x86_64-unknown-linux-gnu
 
