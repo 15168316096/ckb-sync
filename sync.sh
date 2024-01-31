199004b58617a7d73a6219b3acaf1e4d46a492da
sunchengzhu <sunchengzhu@cryptape.com>
2024-01-31 14:42:54 +0800

    调试

--- sync.sh
+++ sync.sh
@@ -20,9 +20,34 @@ else
     toggle_bool
 fi
 
+# 从env中选取testnet或mainnet，以及写入当前日期到env.txt
 env=$(sed -n '1p' env.txt)
 start_date=$(TZ='Asia/Shanghai' date "+%Y-%m-%d")
 sed -i "2s/.*/$start_date/" env.txt
+
+# 定义函数
+killckb() {
+    PROCESS=$(ps -ef | grep /ckb | grep -v grep | awk '{print $2}' | sed -n '2,10p')
+    for i in $PROCESS; do
+        echo "killed the ckb $i"
+        sudo kill -9 $i
+    done
+}
+
+toggle_bool() {
+    # 获取 env.txt 文件的第三行
+    local third_line=$(sed -n '3p' env.txt)
+
+    if [ "$third_line" = "1" ]; then
+        # 如果第三行是 1，则替换为 0
+        sed -i "3s/.*/0/" env.txt
+    elif [ "$third_line" = "0" ]; then
+        # 如果第三行是 0，则替换为 1
+        sed -i "3s/.*/1/" env.txt
+    else
+        echo "第三行既不是1也不是0，未做任何更改"
+    fi
+}
 
 ckb_version=$(curl -s https://api.github.com/repos/nervosnetwork/ckb/releases/latest | jq -r '.tag_name')
 tar_name="ckb_${ckb_version}_x86_64-unknown-linux-gnu.tar.gz"
@@ -70,26 +95,3 @@ grep 'spec =' ckb.toml >>../result_${start_date}.log
 sudo nohup ./ckb run >/dev/null 2>&1 &
 sync_start=$(TZ='Asia/Shanghai' date "+%Y-%m-%d %H:%M:%S")
 echo "sync_start: ${sync_start}" >>../result_${start_date}.log
-
-killckb() {
-    PROCESS=$(ps -ef | grep /ckb | grep -v grep | awk '{print $2}' | sed -n '2,10p')
-    for i in $PROCESS; do
-        echo "killed the ckb $i"
-        sudo kill -9 $i
-    done
-}
-
-toggle_bool() {
-    # 获取 env.txt 文件的第三行
-    local third_line=$(sed -n '3p' env.txt)
-
-    if [ "$third_line" = "1" ]; then
-        # 如果第三行是 1，则替换为 0
-        sed -i "3s/.*/0/" env.txt
-    elif [ "$third_line" = "0" ]; then
-        # 如果第三行是 0，则替换为 1
-        sed -i "3s/.*/1/" env.txt
-    else
-        echo "第三行既不是1也不是0，未做任何更改"
-    fi
-}
