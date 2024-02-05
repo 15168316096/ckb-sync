0a22c7ff48d0ac974fdb06e2a185046533860da4
sunchengzhu <sunchengzhu@cryptape.com>
2024-02-05 22:52:40 +0800

    还是要curl和ping

--- kubernetes/package.sh
+++ kubernetes/package.sh
@@ -10,10 +10,32 @@ docker images --format '{{.Repository}}:{{.Tag}}' | grep '^nervos/ckb:' | while 
     docker rmi "$image"
 done
 
+docker images --format '{{.Repository}}:{{.Tag}}' | grep '^ckb:' | while read -r image; do
+    echo "Deleting image: $image"
+    docker rmi "$image"
+done
+
 docker images --format '{{.Repository}}:{{.Tag}}' | grep '^registry.cn-hangzhou.aliyuncs.com/scz996/ckb:' | while read -r image; do
     echo "Deleting image: $image"
     docker rmi "$image"
 done
+
+# 提取镜像名称和标签
+ckb_image=$(docker images --format '{{.Repository}}:{{.Tag}}\t{{.CreatedAt}}' | grep '^nervos/ckb:' | sort -k 2 -r | head -n 1 | cut -f1)
+echo $ckb_image
+
+# 创建 Dockerfile
+cat <<EOF >Dockerfile
+FROM $ckb_image
+USER root
+RUN mkdir -p /var/lib/apt/lists/partial
+RUN chmod -R 777 /var/lib/apt/lists
+RUN apt-get update && apt-get install -y \\
+    curl \\
+    iputils-ping
+EOF
+
+cat Dockerfile
 
 # https://github.com/eval-exec/ckb/tree/exec/async
 # 做镜像
@@ -22,12 +44,13 @@ cd ckb
 git checkout exec/async
 make docker
 
-# 上传镜像
 commit_version=$(git log -n 1 --pretty=format:"%h")
 date=$(TZ='Asia/Shanghai' date "+%Y-%m-%d")
+sudo docker build -t ckb:async-${date}-${commit_version} .
 
+# 上传镜像
 export DOCKER_PASSWORD=CkbSync02
 echo $DOCKER_PASSWORD | docker login --username 肥宅小竹 --password-stdin registry.cn-hangzhou.aliyuncs.com
-image_id=$(docker images | awk '$1=="nervos/ckb" {print $3}')
+image_id=$(docker images | grep "ckb.*async-${date}-${commit_version}" | awk '{print $3}')
 docker tag $image_id registry.cn-hangzhou.aliyuncs.com/scz996/ckb:async-${date}-${commit_version}
 docker push registry.cn-hangzhou.aliyuncs.com/scz996/ckb:async-${date}-${commit_version}
